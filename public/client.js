c507f4844ba37852c15b755c0bbad39b32d4b59b
Hector Zelaya <inge.zelaya@gmail.com>
2017-10-25 14:41:00 -0600

    added stream options for call

--- public/client.js
+++ public/client.js
@@ -1,38 +1,41 @@
 // getting dom elements
 var divSelectRoom = document.getElementById("selectRoom");
-var divConsultingRoom = document.getElementById("consultingRoom");
-var inputRoomNumber = document.getElementById("roomNumber");
-var btnGoRoom = document.getElementById("goRoom");
+var divConferenceRoom = document.getElementById("conferenceRoom");
+var btnGoBoth = document.getElementById("goBoth");
+var btnGoVideoOnly = document.getElementById("goVideoOnly");
 var localVideo = document.getElementById("localVideo");
 var remoteVideo = document.getElementById("remoteVideo");
+var listAudioEvents = document.getElementById("audioEvents");
 
 // variables
-var roomNumber;
+var roomNumber = 'webrtc-audio-demo';
 var localStream;
 var remoteStream;
 var rtcPeerConnection;
 var iceServers = {
-    'iceServers': [
-        { 'url': 'stun:stun.services.mozilla.com' },
-        { 'url': 'stun:stun.l.google.com:19302' }
+    'iceServers': [{
+            'url': 'stun:stun.services.mozilla.com'
+        },
+        {
+            'url': 'stun:stun.l.google.com:19302'
+        }
     ]
 }
-var streamConstraints = { audio: false, video: true };
+var streamConstraints;
 var isCaller;
 
 // Let's do this
 var socket = io();
 
-btnGoRoom.onclick = function () {
-    if (inputRoomNumber.value === '') {
-        alert("Please type a room number")
-    } else {
-        roomNumber = inputRoomNumber.value;
-        socket.emit('create or join', roomNumber);
-        divSelectRoom.style = "display: none;";
-        divConsultingRoom.style = "display: block;";
-    }
-};
+btnGoBoth.onclick = () => initiateCall(true);
+btnGoVideoOnly.onclick = () => initiateCall(false);
+
+function initiateCall(audio) {
+    streamConstraints = {video: true, audio: audio}
+    socket.emit('create or join', roomNumber);
+    divSelectRoom.style = "display: none;";
+    divConferenceRoom.style = "display: block;";
+}
 
 // message handlers
 socket.on('created', function (room) {
@@ -69,22 +72,26 @@ socket.on('ready', function () {
         rtcPeerConnection.onicecandidate = onIceCandidate;
         rtcPeerConnection.onaddstream = onAddStream;
         rtcPeerConnection.addStream(localStream);
-        rtcPeerConnection.createOffer(setLocalAndOffer, function(e){console.log(e)});
+        rtcPeerConnection.createOffer(setLocalAndOffer, function (e) {
+            console.log(e)
+        });
     }
 });
 
-socket.on('offer', function (event){
-    if(!isCaller){
+socket.on('offer', function (event) {
+    if (!isCaller) {
         rtcPeerConnection = new RTCPeerConnection(iceServers);
         rtcPeerConnection.onicecandidate = onIceCandidate;
         rtcPeerConnection.onaddstream = onAddStream;
         rtcPeerConnection.addStream(localStream);
         rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
-        rtcPeerConnection.createAnswer(setLocalAndAnswer, function(e){console.log(e)});
+        rtcPeerConnection.createAnswer(setLocalAndAnswer, function (e) {
+            console.log(e)
+        });
     }
 });
 
-socket.on('answer', function (event){
+socket.on('answer', function (event) {
     rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
 })
 
@@ -105,6 +112,14 @@ function onIceCandidate(event) {
 function onAddStream(event) {
     remoteVideo.src = URL.createObjectURL(event.stream);
     remoteStream = event.stream;
+    if(remoteStream.getAudioTracks().length > 0) {
+        addAudioEvent('Remote user is sending Audio');
+        addAudioEvent(remoteStream.getAudioTracks()[0].enabled ?
+                            "Remote user's audio is unmuted" :
+                            "Remote user's audio is muted");
+    } else {
+        addAudioEvent('Remote user is not sending Audio');
+    }
 }
 
 function setLocalAndOffer(sessionDescription) {
@@ -123,4 +138,10 @@ function setLocalAndAnswer(sessionDescription) {
         sdp: sessionDescription,
         room: roomNumber
     });
+}
+
+function addAudioEvent(event) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(event));
+    listAudioEvents.appendChild(li);
 }
